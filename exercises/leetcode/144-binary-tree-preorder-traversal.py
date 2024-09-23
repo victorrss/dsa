420712cd6371fa74ad33a44d41ffb61d20baecf3
Victor Rubens <victorrss@live.com>
2024-09-23 00:36:21 -0300

    Create 144-binary-tree-preorder-traversal.py

---
+++ exercises/leetcode/144-binary-tree-preorder-traversal.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def traverse(self, root, result):
+        if not root:
+            return result
+
+        result.append(root.val)
+        self.traverse(root.left, result)
+        self.traverse(root.right, result)
+    
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        self.traverse(root, result)
+        return result
