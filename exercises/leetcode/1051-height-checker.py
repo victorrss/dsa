210aa0860b2cb48826aaaf2738280053997499aa
Victor Rubens <victorrss@live.com>
2024-11-03 14:02:49 -0300

    Create 1051-height-checker.py

---
+++ exercises/leetcode/1051-height-checker.py
@@ -0,0 +1,10 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = heights.copy()
+        expected.sort()
+
+        count = 0
+        for i in range(len(heights)):
+            if expected[i] != heights[i]:
+                count += 1
+        return count
